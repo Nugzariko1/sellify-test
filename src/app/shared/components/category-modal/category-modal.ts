18148bf9aeb128d969c23fd9d6297510903638be
Luka Jamrishvili <luka.jamrishvili@gmail.com>
2026-01-20 01:29:14 +0400

    unnamed commit

--- src/app/shared/components/category-modal/category-modal.ts
+++ src/app/shared/components/category-modal/category-modal.ts
@@ -1,15 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-category-modal',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './category-modal.html',
   styleUrl: './category-modal.scss',
 })
 export class CategoryModal {
-  loginForm: any;
+  createCategoryForm: FormGroup;
+
+  constructor() {
+    this.createCategoryForm = new FormBuilder().group({
+      name: ['', Validators.required],
+      description: [''],
+      slug: [''],
+      parentId: [''],
+      isActive: [true],
+      iconFile: [null],
+    });
+  }
+
   closeModal() {
     const modalCont: any = document.querySelector('.modal-container')!;
     modalCont.style.display = 'none';
   }
+
+  createCategory() {}
 }
