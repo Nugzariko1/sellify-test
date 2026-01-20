7ebe8b855d63cda82a5f43e2645c700a7495cf69
lukajoo1 <lukajokhadze97@gmail.com>
2026-01-20 06:55:30 -0800

    change category card styles

--- src/app/features/auth/log-in/log-in.ts
+++ src/app/features/auth/log-in/log-in.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LogInRequest } from '../../../shared/types/auth.models';
 import { AuthService } from '../../../shared/services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
@@ -18,6 +19,7 @@ export class LogIn {
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
+    private router: Router,
   ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
@@ -33,18 +35,22 @@ export class LogIn {
   }
 
   async login() {
+    const loginMessage: any = document.querySelector('.for-login');
     const request: LogInRequest = {
       email: this.loginForm.value.email,
       password: this.loginForm.value.password,
     };
     this.authService.logIn(`${this.apiUrl}/log-in`, request).subscribe({
       next: (response) => {
+        this.router.navigate(['']);
         console.log('Login successful:', response);
         localStorage.setItem('accessToken', response.data.accessToken);
         localStorage.setItem('refreshToken', response.data.refreshToken);
       },
       error: (error) => {
         console.error('Login failed:', error);
+        loginMessage.innerText = 'Invalid credentials';
+        loginMessage.style.color = 'red';
       },
     });
   }
